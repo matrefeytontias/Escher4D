065458b053b8132ab8c8655b175d442951c78b9a
matrefeytontias <mattias@refeyton.fr>
2019-01-02 20:55:01 +0100

    Added sample models

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 #include <GLFW/glfw3.h>
 
 #include "Camera4.hpp"
+#include "OFFLoader.hpp"
 #include "Model4RenderContext.hpp"
 #include "ModelGroup.hpp"
 #include "ShaderProgram.hpp"
@@ -40,38 +41,6 @@ void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods)
 {
     ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mods);
 }
-
-// Vertices of a cube
-static vector<Vector3f> cubeVertices({
-	Vector3f(-.5f, -.5f, .5f),
-	Vector3f(.5f, -.5f, .5f),
-	Vector3f(.5f, .5f, .5f),
-	Vector3f(-.5f, .5f, .5f),
-	Vector3f(-.5f, -.5f, -.5f),
-	Vector3f(.5f, -.5f, -.5f),
-	Vector3f(.5f, .5f, -.5f),
-	Vector3f(-.5f, .5f, -.5f)
-});
-
-// Triangulation of the surface of a cube
-static vector<unsigned int> cubeTris({
-    0, 1, 2, 0, 2, 3,
-    4, 6, 5, 4, 7, 6,
-    0, 3, 7, 0, 7, 4,
-    1, 6, 2, 1, 5, 6,
-    2, 6, 7, 2, 7, 3,
-    1, 0, 4, 1, 4, 5
-});
-
-// Tetrahedrization of a cube
-static vector<unsigned int> cubeTetras({
-	0, 3, 5, 4,
-	1, 5, 3, 0,
-	3, 5, 7, 4,
-	1, 3, 6, 5,
-	2, 6, 3, 1,
-	3, 6, 7, 5
-});
 
 int _main(int, char *argv[])
 {
@@ -112,43 +81,58 @@ int _main(int, char *argv[])
     glEnable(GL_DEPTH_TEST);
     
     Camera4 camera(window);
-    camera.pos << 0, 1.5, 3, 0;
+    // camera.pos << 0, 1.5, 3, 0;
+    camera.pos(2) = 5;
     
     Eigen::Matrix4f p(Eigen::Matrix4f::Identity());
-    ShaderProgram cubeProgram;
-    cubeProgram.attach(GL_VERTEX_SHADER, "shaders/vertex.glsl");
-    cubeProgram.attach(GL_GEOMETRY_SHADER, "shaders/geometry.glsl");
-    cubeProgram.attach(GL_FRAGMENT_SHADER, "shaders/fragment.glsl");
-    Geometry4 cubeGeometry = Geometry4::from3D(cubeVertices, cubeTris, cubeTetras, 1);
-    cubeGeometry.unindex();
-    cubeGeometry.recomputeNormals();
-    cubeGeometry.uploadGPU();
+    ShaderProgram program;
+    program.attach(GL_VERTEX_SHADER, "shaders/vertex.glsl");
+    program.attach(GL_GEOMETRY_SHADER, "shaders/geometry.glsl");
+    program.attach(GL_FRAGMENT_SHADER, "shaders/fragment.glsl");
+    std::vector<Eigen::Vector3f> vertices;
+    std::vector<unsigned int> tris, tetras;
+    if(!OFFLoader::loadModel("models/socket", vertices, tris, tetras))
+    {
+        trace("Can't load that shizzle");
+        return 0;
+    }
+    Geometry4 geometry = Geometry4::from3D(vertices, tris, tetras, 1);
+    trace("Model has " << geometry.cells.size() << " tetrahedra in it");
+    geometry.unindex();
+    geometry.recomputeNormals();
+    geometry.uploadGPU();
     
     // Let's construct the scene
     ModelGroup scene;
-    Model4RenderContext wall(cubeGeometry, cubeProgram);
+    /**
+    Model4RenderContext wall(geometry, program);
     wall.scale(Vector4f(20, 2.5, 1, 1));
     wall.pos << 0, 1.2, 0, 0;
     wall.color << 1, 1, 1, 1;
     
-    Model4RenderContext noCube(cubeGeometry, cubeProgram);
+    Model4RenderContext noCube(geometry, program);
     noCube.pos << -7, 0.5, 2, 0;
     noCube.color << 1, 0, 0, 1;
-    Model4RenderContext yesCube(cubeGeometry, cubeProgram);
+    Model4RenderContext yesCube(geometry, program);
     yesCube.pos << -7, 0.5, -6, 0;
     yesCube.color << 0, 1, 0, 1;
     
-    Model4RenderContext negGround(cubeGeometry, cubeProgram);
+    Model4RenderContext negGround(geometry, program);
     negGround.scale(Vector4f(20, 1, 20, 10));
     negGround.pos << 0, -0.5, 0, -5;
     negGround.color << 0.75, 0.75, 1., 1.;
     
-    Model4RenderContext posGround(cubeGeometry, cubeProgram);
+    Model4RenderContext posGround(geometry, program);
     posGround.scale(Vector4f(20, 1, 20, 10));
     posGround.pos << 0, -0.5, 0, 5;
     posGround.color << 1, 0.75, 0.75, 1;
     
     scene.add(wall).add(noCube).add(yesCube).add(negGround).add(posGround);
+    */
+    Model4RenderContext obj(geometry, program);
+    obj.scale(Vector4f(0.05, 0.05, 0.05, 1));
+    obj.color << 1, 1, 1, 1;
+    scene.add(obj);
     
     perspective(p, 90, (float)display_w / display_h, 0.0001, 100);
     
@@ -170,14 +154,16 @@ int _main(int, char *argv[])
         timeBase = now;
         ImGui_ImplGlfwGL3_NewFrame();
         
+        obj.rotate(ZW, dt);
+        
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        cubeProgram.use();
+        program.use();
         
-        cubeProgram.uniformMatrix4fv("P", 1, &p.data()[0]);
-        cubeProgram.uniform1f("uLightIntensity", lightIntensity);
-        cubeProgram.uniform1f("uLightRadius", lightRadius);
-        cubeProgram.uniform3f("uColor", 1, 1, 1);
+        program.uniformMatrix4fv("P", 1, &p.data()[0]);
+        program.uniform1f("uLightIntensity", lightIntensity);
+        program.uniform1f("uLightRadius", lightRadius);
+        program.uniform3f("uColor", 1, 1, 1);
         
         // Render the scene
         scene.render(camera);
@@ -214,7 +200,6 @@ int _main(int, char *argv[])
     }
     
     trace("Exiting drawing loop");
-    
     // Cleanup
     ImGui_ImplGlfwGL3_Shutdown();
     ImGui::DestroyContext();
